dd64de8b96bb2fdf31cbb96b79458970efd2702d
TrshCan <phuthuytinhyeu1234678@gmail.com>
2025-10-18 13:53:14 +0700

    completed Models for Post, PostLike, PostShare, Event and Deadline

---
+++ be/app/Models/Deadline.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Deadline extends Model
+{
+    use SoftDeletes;
+
+    public $timestamps = false;
+
+    protected $fillable = ['title', 'deadline_date', 'details', 'created_at', 'created_by'];
+    protected $casts = [
+        'deadline_date' => 'date',
+        'created_at' => 'datetime',
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
