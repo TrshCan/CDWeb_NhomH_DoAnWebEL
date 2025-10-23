13c6c8cc6bb9922b156ffc9f6a8b9136a83bf804
Tôm <bthvuong23@gmail.com>
2025-10-23 16:45:08 +0700

    Add create survey question templates

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Register from './pages/Register.jsx'
+// 1. ĐỔI TÊN KHI IMPORT: "c" thành "C"
+import CreateSurveyQuestion from './pages/CreateSurveyQuestion.jsx' 
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Register />
+    {/* 2. ĐỔI TÊN KHI RENDER: "c" thành "C" */}
+    <CreateSurveyQuestion />
   </StrictMode>,
 )
