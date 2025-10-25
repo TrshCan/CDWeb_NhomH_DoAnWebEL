2d9ee18cf0e813040e09d0008e79f2805b1f25c2
nbaonguyen111 <baonguyenmiennam@gmail.com>
2025-10-25 15:25:31 +0700

    Add avatar table

--- be/app/Repositories/UserRepository.php
+++ be/app/Repositories/UserRepository.php
@@ -15,11 +15,19 @@ class UserRepository {
     public function registerUser(array $data): User
     {
         $user = new User($data);
+        $user->name = $data['name'];
         $user->email = $data['email'];
         $user->phone =$data['phone'];
         $user->address =$data['address'];
         $user->password = Hash::make($data['password']);
-        $user->status_id = 1;
+        $user->status_id   = $data['status_id'] ?? 1;
+        $user->role        = $data['role']        ?? 'student';
+        $user->class_id    = $data['class_id']    ?? null;
+        $user->faculty_id  = $data['faculty_id']  ?? null;
+        $user->ban_reason  = $data['ban_reason']  ?? null;
+        $user->point       = $data['point']       ?? 0;
+        $user->email_verified_at = null;
+        $user->avatar      = $data['avatar']   ?? 'default.png';
         $user->save();
         $user->refresh();
         return $user;
