a10bb068f5b40d393d7b10d6c8bf61c162a3e1dd
nbaonguyen111 <baonguyenmiennam@gmail.com>
2025-10-27 18:28:16 +0700

    delete routes

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import Register from './pages/Register';
+import Login from './pages/Login.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+
   );
 }
