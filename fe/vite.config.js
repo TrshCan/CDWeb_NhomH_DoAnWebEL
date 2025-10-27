68ae15b76b13acbf978ea01acf91feb334541672
TrshCan <phuthuytinhyeu1234678@gmail.com>
2025-10-27 23:01:25 +0700

    minor fixes

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  
+  resolve: {
+    alias: {
+      "lucide-react": "lucide-react/dist/esm/lucide-react.js",
+    },
+  },
 })
