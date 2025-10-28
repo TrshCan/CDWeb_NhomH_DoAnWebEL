27d19d48afebc8d3e19d4279a5233a3bf323ab4a
TrshCan <phuthuytinhyeu1234678@gmail.com>
2025-10-28 14:25:14 +0700

    updated to use outlet
    Signed-off-by: TrshCan <phuthuytinhyeu1234678@gmail.com>

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,15 +1,19 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage';
+import Feed from './components/Feed';
 import Register from './pages/Register';
-import Search from './pages/SearchPage';
-
+import Search from './components/SearchResult';
+import MainLayout from './layouts/MainLayout';
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+         <Route path="/" element={<MainLayout />}>
+          {/* 👇 These are children rendered inside <Outlet /> */}
+          <Route index element={<Feed />} />           {/* / */}
+          <Route path="explore" element={<Search />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/search" element={<Search />} />
       </Routes>
