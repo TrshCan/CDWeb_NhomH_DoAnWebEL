c1f57bf31b7a4942294ab39af6d29297c57c20af
TrshCan <phuthuytinhyeu1234678@gmail.com>
2025-10-30 16:30:24 +0700

    made change in app
    Signed-off-by: TrshCan <phuthuytinhyeu1234678@gmail.com>

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,24 +1,26 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import Register from './pages/Register';
-import Login from './pages/Login.jsx';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Register from "./pages/Register";
+import Login from "./pages/Login.jsx";
 import Feed from "./components/Feed.jsx";
 import Profile from "./pages/Profile.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 
 export default function App() {
   return (
     <Routes>
-        {/* Layout chính */}
-        <Route path="/" element={<MainPage />} >
-            {/* các route con  */}
-          <Route index element={<Feed />} /> {/* mặc định là Feed */}
-          <Route path="profile" element={<Profile />} />
-            {/* sau này sẽ thêm các route như profile expole v.v*/}
+      {/* Layout chính */}
+      <Route path="/" element={<MainLayout />}>
+        {/* các route con  */}
+        <Route index element={<Feed />} /> {/* mặc định là Feed */}
+        <Route path="profile" element={<Profile />} />
+        {/* sau này sẽ thêm các route như profile expole v.v*/}
       </Route>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+      
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
